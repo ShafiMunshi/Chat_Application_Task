ce9c1ddc24a76da7c5232154e103979d41aa31a9
ShafiMunshi <shafimunshi111@gmail.com>
2026-02-24 14:14:14 +0600

    screen util package added

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,6 +1,7 @@
 import 'package:chat_application_task/core/routes/route.dart';
 import 'package:chat_application_task/features/auth/views/screens/sign_in_screen.dart';
 import 'package:chat_application_task/features/auth/views/screens/sign_up_screen.dart';
+import 'package:chat_application_task/features/chat/views/widgets/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,9 +14,7 @@ class MainApp extends ConsumerWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.dark,
       routerConfig: router,
     );
   }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,11 +2,23 @@ import 'package:chat_application_task/core/config/firebase_initializer.dart';
 import 'package:chat_application_task/main_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
     await FirebaseInitializer.init();
 
-  runApp(const ProviderScope(child: MainApp()));
+  runApp(
+    ProviderScope(
+      child: ScreenUtilInit(
+        designSize: const Size(390, 844),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return const MainApp();
+        },
+      ),
+    ),
+  );
 }
