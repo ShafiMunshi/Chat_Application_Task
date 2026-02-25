3dd47e1851e757550a0abcf50ead490b5586af28
ShafiMunshi <shafimunshi111@gmail.com>
2026-02-25 19:43:49 +0600

    initial workable firebase initialize - fixed all type of version related issue..

--- android/app/src/main/kotlin/com/example/chat_application_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chat_application_task/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.example.chat_application_task
 
+import com.google.firebase.firestore.FirebaseFirestore
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private lateinit var firestore: FirebaseFirestore
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        firestore = FirebaseFirestore.getInstance()
+    }
+}
